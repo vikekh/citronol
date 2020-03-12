4bcb81439ea6dc654a0e830b2f01a9418d3a46e9
vikekh <621212+vikekh@users.noreply.github.com>
2020-03-12 19:08:21 +0100

    ASP.NET Core hosted

--- src/Vikekh.Citronol.Client/Program.cs
+++ src/Vikekh.Citronol.Client/Program.cs
@@ -1,16 +1,20 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Vikekh.Citronol.Client
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+   public class Program
+   {
+      public static async Task Main(string[] args)
+      {
+         var builder = WebAssemblyHostBuilder.CreateDefault(args);
+         builder.RootComponents.Add<App>("app");
 
-            await builder.Build().RunAsync();
-        }
-    }
+         await builder.Build().RunAsync();
+      }
+   }
 }
---
+++ src/Vikekh.Citronol.Server/Controllers/CountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Vikekh.Citronol.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CountController : ControllerBase
+    {
+        private readonly ILogger<CountController> _logger;
+
+        public CountController(ILogger<CountController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public int Count()
+        {
+            return 94;
+        }
+    }
+}
